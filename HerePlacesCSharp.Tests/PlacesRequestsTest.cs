172bfcdc4d94c2893f9c753487f7d664c8c97a57
Yurii Sydorets <Sirnick@ukr.net>
2019-01-20 14:52:16 +0200

    Added testst

---
+++ HerePlacesCSharp.Tests/PlacesRequestsTest.cs
@@ -0,0 +1,57 @@
+﻿// TODO: Mock places service 
+
+using HerePlacesCSharp.Model;
+using HerePlacesCSharp.Services;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HerePlacesCSharp.Tests
+{
+    [TestClass]
+    public class PlacesRequestsTest
+    {
+        private PlacesService placesService = new PlacesService("api key", "api token");
+
+        [TestMethod]
+        public void DiscoverSearchPlacesFound()
+        {
+            var request = new PlacesRequest { Q = "antipodes", At = "52.531,13.3843" };
+
+            var response = placesService.DiscoverSearchPlaces(request).Result;
+            Assert.IsTrue(response.Count > 0);
+        }
+
+        [TestMethod]
+        public void DiscoverExplorePlacesFound()
+        {
+            var request = new PlacesRequest { At = "52.531,13.3843" };
+
+            var response = placesService.DiscoverExplorePlaces(request).Result;
+            Assert.IsTrue(response.Count > 0);
+        }
+
+        [TestMethod]
+        public void LookupPlacesFound()
+        {
+            var request = new PlacesRequest { Source = "sharing", PlaceId = "276u33db-bae64cc3795443a381643b996f02a2a5" };
+
+            var response = placesService.LookupPlaces(request).Result;
+            Assert.IsTrue(response != null);
+        }
+
+        [TestMethod]
+        public void BrowsePlacesFound()
+        {
+            var request = new PlacesRequest { At = "52.531,13.3843" };
+
+            var response = placesService.BrowsePlaces(request).Result;
+            Assert.IsTrue(response.Count > 0);
+        }
+
+        [TestMethod]
+        public void HealthCheck()
+        {
+            placesService.HealthCheck();
+        }
+    }
+}
---
+++ HerePlacesCSharp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HerePlacesCSharp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HerePlacesCSharp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d468c39d-94bc-4386-a146-556114752030")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
